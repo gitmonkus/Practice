d54b84312a0b8c949f6df8130bd98c45c9e8c761
ironmonkus <ironmonkus@gmail.com>
2018-04-27 16:02:38 -0600

    New practice.py file

---
+++ practice.py
@@ -0,0 +1,15 @@
+# my_list = ["fish", "fun", 7, "taco"]
+# #print(my_list)
+# my_list.append("burrito")
+# print(my_list)
+# del my_list[2]
+# print(my_list)
+
+print("Hello \
+What is shaking and baking?")
+print("\a")
+print("\tHermit")
+print("time oh yeah!")
+
+
+
