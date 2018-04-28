6fa9e36418e779170e0a8b5b3e0f0d316daf603c
ironmonkus <cybermonkus@MACINTOSH.local>
2018-04-27 19:20:58 -0600

    Added the hello.py file

---
+++ hello.py
@@ -0,0 +1,33 @@
+# f = 5
+# g = 6
+# s = f*g
+# print("%s is the luckiest number that you've ever seen!" % s)
+
+
+# mylist = ["fish", "tacos", "peaches", "mangos", "peanut"]
+# print(mylist)
+# print(mylist[2])
+# mylist[2] = "plums"
+# print(mylist[2])
+# print(mylist)
+# mylist.append("fuzz")
+# print(mylist)
+# mylist.remove("fish")
+# print(mylist)
+
+# f = 4
+# total = f
+# g = 5
+# total += g
+# print(total)
+# t = 1
+# total -= t
+# print(total)
+
+day = 6
+print("03 - %s - 2019" % day)
+print("03 - %02d - 2019" % day)
+
+
+
+
