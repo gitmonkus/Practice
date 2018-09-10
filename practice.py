b93348f3d3e1eb730671ad2043b51e2e5e0e1112
ironmonkus <ironmonkus@gmail.com>
2018-09-10 15:49:30 -0600

    uploading practice.py

--- practice.py
+++ practice.py
@@ -18,16 +18,27 @@
 # t = (f*g)
 # print(t)
 
-def fishsticks(stuff):
-    pie = "You love the fishy {}?".format(stuff)
-    return pie
+# def fishsticks(stuff):
+#     pie = "You love the fishy {}?".format(stuff)
+#     return pie
 
-ask = input("What is your favorite food?")
-ask1 = input()
-food = fishsticks(ask)
+# ask = input("What is your favorite food?")
+# ask1 = input()
+# food = fishsticks(ask)
 
-print(food)
-print(len(ask1))
+# print(food)
+# print(len(ask1))
+
+twinkies = input(int("What quantity of twinkies do you shove in your mouth \n \n"))
+
+if twinkies < 100:
+    print("Too few in your mouth")
+    
+elif twinkies > 500:
+    print("Too many in your mouth")
+    
+else:
+    print("You crazy with those twinkies in your mouth")
 
 
 
