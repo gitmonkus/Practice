acfb26fff1d994c232f18cb109045d64e6caecd6
ironmonkus <ironmonkus@gmail.com>
2018-09-10 20:39:03 -0600

    adding chapter_7.py

---
+++ chapter_7.py
@@ -0,0 +1,14 @@
+# def moon_weight():
+#     weight = int(input("How much do you weigh?\n> "))
+#     years = int(input("How many years are you going to be on the moon?\n> "))
+#     increase = int(input("You put the cookies in there, How many times in a year?\n> "))
+
+#     for year in range(1, years + 1):
+#         weight = weight + increase
+#         print("Afer year" , year , "of eating cookies, you weigh" , weight , "lbs")
+#         if (year == years):
+#             print("\n\nSHOVE SOME FU$#%&@ LETTUCE IN THERE!\n\n")
+
+# moon_weight()
+
+
